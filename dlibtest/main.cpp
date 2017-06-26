a6bd4a6e374b472af0cba1a763c5666c7cc737c7
Nutchaphon Saengboon <nutchaphon.saengboon@thomsonreuters.com>
2017-06-26 10:11:53 +0700

    test stage

--- dlibtest/main.cpp
+++ dlibtest/main.cpp
@@ -1,5 +1,161 @@
+#include <iostream>
+#include <dlib/svm.h>
+#include <vector>
+#include <dlib/rand.h>
+#include <dlib/svm_threaded.h>
+
+using namespace std;
+using namespace dlib;
 
 
+int main()
+{
+	typedef matrix<double, 2, 1> sample_type;
+
+	typedef radial_basis_kernel<sample_type> kernel_type;
+
+
+	std::vector<sample_type> samples;
+	std::vector<double> labels;
+	
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	wstring ws(buffer);
+	string path(ws.begin(), ws.end());
+	path = path.substr(0, path.find_last_of("\\/") + 1);
+
+
+
+
+	for (int r = -20; r <= 20; ++r)
+	{
+		for (int c = -20; c <= 20; ++c)
+		{
+			sample_type samp;
+			samp(0) = r;
+			samp(1) = c;
+			samples.push_back(samp);
+
+			// if this point is less than 10 from the origin
+			if (sqrt((double)r*r + c*c) <= 10)
+				labels.push_back(+1);
+			else
+				labels.push_back(-1);
+
+		}
+	}
+ 
+	vector_normalizer<sample_type> normalizer;
+	// let the normalizer learn the mean and standard deviation of the samples
+	normalizer.train(samples);
+	// now normalize each sample
+	for (unsigned long i = 0; i < samples.size(); ++i)
+		samples[i] = normalizer(samples[i]);
+
+	randomize_samples(samples, labels);
+
+	const double max_nu = maximum_nu(labels);
+
+	// here we make an instance of the svm_nu_trainer object that uses our kernel type.
+	svm_nu_trainer<kernel_type> trainer;
+
+
+	cout << "doing cross validation" << endl;
+	for (double gamma = 0.00001; gamma <= 1; gamma *= 5)
+	{
+		for (double nu = 0.00001; nu < max_nu; nu *= 5)
+		{
+			// tell the trainer the parameters we want to use
+			trainer.set_kernel(kernel_type(gamma));
+			trainer.set_nu(nu);
+
+			cout << "gamma: " << gamma << "    nu: " << nu;
+			
+			cout << "     cross validation accuracy: " << cross_validate_trainer(trainer, samples, labels, 3);
+		}
+	}
+
+	trainer.set_kernel(kernel_type(0.15625));
+	trainer.set_nu(0.15625);
+	typedef decision_function<kernel_type> dec_funct_type;
+	typedef normalized_function<dec_funct_type> funct_type;
+
+	funct_type learned_function;
+	learned_function.normalizer = normalizer;  // save normalization information
+	learned_function.function = trainer.train(samples, labels); // perform the actual SVM training and save the results
+
+																// print out the number of support vectors in the resulting decision function
+	cout << "\nnumber of support vectors in our learned_function is "
+		<< learned_function.function.basis_vectors.size() << endl;
+
+	// Now let's try this decision_function on some samples we haven't seen before.
+	sample_type sample;
+
+	sample(0) = 3.123;
+	sample(1) = 2;
+	cout << "This is a +1 class example, the classifier output is " << learned_function(sample) << endl;
+
+	sample(0) = 3.123;
+	sample(1) = 9.3545;
+	cout << "This is a +1 class example, the classifier output is " << learned_function(sample) << endl;
+
+	sample(0) = 13.123;
+	sample(1) = 9.3545;
+	cout << "This is a -1 class example, the classifier output is " << learned_function(sample) << endl;
+
+	sample(0) = 13.123;
+	sample(1) = 0;
+	cout << "This is a -1 class example, the classifier output is " << learned_function(sample) << endl;
+
+	typedef probabilistic_decision_function<kernel_type> probabilistic_funct_type;
+	typedef normalized_function<probabilistic_funct_type> pfunct_type;
+
+	pfunct_type learned_pfunct;
+	learned_pfunct.normalizer = normalizer;
+	learned_pfunct.function = train_probabilistic_decision_function(trainer, samples, labels, 3);
+	
+	cout << "\nnumber of support vectors in our learned_pfunct is "
+		<< learned_pfunct.function.decision_funct.basis_vectors.size() << endl;
+
+	sample(0) = 3.123;
+	sample(1) = 2;
+	cout << "This +1 class example should have high probability.  Its probability is: "
+		<< learned_pfunct(sample) << endl;
+
+	sample(0) = 3.123;
+	sample(1) = 9.3545;
+	cout << "This +1 class example should have high probability.  Its probability is: "
+		<< learned_pfunct(sample) << endl;
+
+	sample(0) = 13.123;
+	sample(1) = 9.3545;
+	cout << "This -1 class example should have low probability.  Its probability is: "
+		<< learned_pfunct(sample) << endl;
+
+	sample(0) = 13.123;
+	sample(1) = 0;
+	cout << "This -1 class example should have low probability.  Its probability is: "
+		<< learned_pfunct(sample) << endl;
+
+	serialize("saved_function.dat") << learned_pfunct;
+
+	// Now let's open that file back up and load the function object it contains.
+	deserialize("saved_function.dat") >> learned_pfunct;
+
+	cout << "\ncross validation accuracy with only 10 support vectors: "
+		<< cross_validate_trainer(reduced2(trainer, 10), samples, labels, 3);
+
+	// Let's print out the original cross validation score too for comparison.
+	cout << "cross validation accuracy with all the original support vectors: "
+		<< cross_validate_trainer(trainer, samples, labels, 3);
+
+	learned_function.function = reduced2(trainer, 10).train(samples, labels);
+	// And similarly for the probabilistic_decision_function: 
+	learned_pfunct.function = train_probabilistic_decision_function(reduced2(trainer, 10), samples, labels, 3);
+}
+
+
+/*
 #include <dlib/svm_threaded.h>
 
 #include <iostream>
@@ -19,13 +175,6 @@ void generate_data(
 	std::vector<sample_type>& samples,
 	std::vector<double>& labels
 );
-/*!
-ensures
-- make some 3 class data as described above.
-- Create 60 points from class 1
-- Create 70 points from class 2
-- Create 80 points from class 3
-!*/
 
 // ----------------------------------------------------------------------------------------
 
@@ -35,7 +184,7 @@ int main()
 	{
 
 
-		std::vector<sample_type> samples, fileA, fileB, fileC, test;
+		std::vector<sample_type> samples, fileA, fileB, fileC, fileD, test;
 		std::vector<double> labels;
 
 		wchar_t buffer[MAX_PATH];
@@ -48,6 +197,7 @@ int main()
 		deserialize(path + "m.01b5zn.dat") >> fileA;
 		deserialize(path + "m.01b_88.dat") >> fileB;
 		deserialize(path + "m.01b0fq.dat") >> fileC;
+		deserialize(path + "trump.dat") >> fileD;
 
 		for (int i = 0; i < fileA.size(); i++)
 			labels.push_back(1);
@@ -55,15 +205,18 @@ int main()
 			labels.push_back(2);
 		for (int i = 0; i < fileC.size(); i++)
 			labels.push_back(3);
+		for (int i = 0; i < fileD.size(); i++)
+			labels.push_back(4);
 
 		samples.insert(samples.end(), fileA.begin(), fileA.end());
 		samples.insert(samples.end(), fileB.begin(), fileB.end());
 		samples.insert(samples.end(), fileC.begin(), fileC.end());
+		samples.insert(samples.end(), fileD.begin(), fileD.end());
 
-		typedef one_vs_one_trainer<any_trainer<sample_type> > ovo_trainer;
+		typedef one_vs_all_trainer<any_trainer<sample_type> > ova_trainer;
 
 		// Finally, make the one_vs_one_trainer.
-		ovo_trainer trainer;
+		ova_trainer trainer;
 
 		typedef polynomial_kernel<sample_type> poly_kernel;
 		typedef radial_basis_kernel<sample_type> rbf_kernel;
@@ -87,18 +240,17 @@ int main()
 		cout << "A -->" << fileA.size() << endl;
 		cout << "B -->" << fileB.size() << endl;
 		cout << "C -->" << fileC.size() << endl;
+		cout << "D -->" << fileD.size() << endl;
 
 		cout << "cross validation: \n" << cross_validate_multiclass_trainer(trainer, samples, labels, 5) << endl;
 		
-		// Next, if you wanted to obtain the decision rule learned by a one_vs_one_trainer you 
-		// would store it into a one_vs_one_decision_function.
-		one_vs_one_decision_function<ovo_trainer> df = trainer.train(samples, labels);
+		one_vs_all_decision_function<ova_trainer> df = trainer.train(samples, labels);
 
 		cout << "predicted label: " << df(samples[0]) << ", true label: " << labels[0] << endl;
 		cout << "predicted label: " << df(samples[6]) << ", true label: " << labels[6] << endl;
 		
 
-		one_vs_one_decision_function<ovo_trainer,
+		one_vs_all_decision_function<ova_trainer,
 			decision_function<poly_kernel>  // This is the output of the poly_trainer
 		> df2, df3;
 
@@ -119,20 +271,22 @@ int main()
 		cout << "predicted label: " << df3(test[5]) << ", true label: 2" << endl;
 		cout << "predicted label: " << df3(test[6]) << ", true label: 3" << endl;
 		cout << "predicted label: " << df3(test[8]) << ", true label: 3" << endl;
-		cout << "predicted label: " << df3(test[9]) << ", true label: ?" << endl;
-		cout << "predicted label: " << df3(test[10]) << ", true label: ?" << endl;
-		// Test df3 on the samples and labels and print the confusion matrix.
+		cout << "predicted label: " << df3(test[9]) << ", true label: 4" << endl;
+		cout << "predicted label: " << df3(test[10]) << ", true label: 4" << endl;
+
+		one_vs_all_decision_function<ova_trainer>::binary_function_table tmp;
+		tmp = df3.get_binary_decision_functions();
+		cout << tmp.size() << endl;
+		
 		cout << "test deserialized function: \n" << test_multiclass_decision_function(df3, samples, labels) << endl;
 
-		// Finally, if you want to get the binary classifiers from inside a multiclass decision
-		// function you can do it by calling get_binary_decision_functions() like so:
-		one_vs_one_decision_function<ovo_trainer>::binary_function_table functs;
+		
+		one_vs_all_decision_function<ova_trainer>::binary_function_table functs;
 		functs = df.get_binary_decision_functions();
 		cout << "number of binary decision functions in df: " << functs.size() << endl;
-		// The functs object is a std::map which maps pairs of labels to binary decision
-		// functions.  So we can access the individual decision functions like so:
-		decision_function<poly_kernel> df_1_2 = any_cast<decision_function<poly_kernel> >(functs[make_unordered_pair(1, 2)]);
-		//decision_function<rbf_kernel>  df_1_3 = any_cast<decision_function<rbf_kernel>  >(functs[make_unordered_pair(1, 3)]);
+		
+		//decision_function<poly_kernel> df_1_2 = any_cast<decision_function<poly_kernel> >(functs[make_unordered_pair(1, 2)]);
+		
 	
 	}
 	catch (std::exception& e)
@@ -208,3 +362,4 @@ void generate_data(
 
 // ----------------------------------------------------------------------------------------
 
+*/
